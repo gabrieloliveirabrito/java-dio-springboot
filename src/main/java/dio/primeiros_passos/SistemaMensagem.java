f9c5b3f06b53777671504d5e616e303d59e6ac7c
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2025-02-22 16:33:34 -0300

    Add list properties

--- src/main/java/dio/primeiros_passos/SistemaMensagem.java
+++ src/main/java/dio/primeiros_passos/SistemaMensagem.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class SistemaMensagem {
     @Autowired
@@ -15,10 +17,14 @@ public class SistemaMensagem {
     @Value("${teste}")
     private String teste;
 
+    @Value("${valores}")
+    private List<Integer> valores;
+
     public void enviarConfirmacaoCadastro() {
         System.out.println(noreply);
         System.out.println("Seu cadastro foi aprovado");
         System.out.println(teste);
+        System.out.println(valores);
     }
 
     public void enviarMensagemBoasVindas() {

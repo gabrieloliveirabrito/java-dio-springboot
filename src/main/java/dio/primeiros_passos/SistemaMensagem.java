6335a0a35665fa9da7c9a9591d75c66bf4e2bda5
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2025-02-22 16:28:48 -0300

    Add properties values

--- src/main/java/dio/primeiros_passos/SistemaMensagem.java
+++ src/main/java/dio/primeiros_passos/SistemaMensagem.java
@@ -1,6 +1,7 @@
 package dio.primeiros_passos;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,9 +12,13 @@ public class SistemaMensagem {
     @Autowired
     private Remetente techTeam;
 
+    @Value("${teste}")
+    private String teste;
+
     public void enviarConfirmacaoCadastro() {
         System.out.println(noreply);
         System.out.println("Seu cadastro foi aprovado");
+        System.out.println(teste);
     }
 
     public void enviarMensagemBoasVindas() {
